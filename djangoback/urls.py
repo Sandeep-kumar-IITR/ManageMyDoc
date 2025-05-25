11e5ae9b387572fffa4ee2f1629a987850a6c5eb
Sandeep kumar <sandeep_k@ma.iitr.ac.in>
2025-05-25 15:39:13 +0530

    message

---
+++ djangoback/urls.py
@@ -0,0 +1,92 @@
+# """
+# URL configuration for djangoback project.
+
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+
+# from django.contrib import admin
+# from django.urls import path
+# from myapp.views import Listdoc, Detaildoc , CreateDoc , DeleteDoc
+
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+    
+# )
+
+
+
+
+# from myapp.views import RegisterView
+# from myapp.views import LoginView
+# from myapp.views import DashboardView
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path('api/api/auth/register/', RegisterView.as_view(), name='register'),
+#     path('api/api/auth/login/', LoginView.as_view(), name='login'),
+#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     path('api/dashboard/', DashboardView.as_view(), name='dashboard'),
+
+
+
+#     path('ap/', Listdoc.as_view(), name='listdoc'),
+
+
+#     path('ap/<int:pk>/', Detaildoc.as_view(), name='detaildoc'),
+    
+#     path('ap/create/', CreateDoc.as_view(), name='createdoc'),
+    
+#     path('ap/delete/<int:pk>/', DeleteDoc.as_view(), name='deletedoc'),
+
+
+
+# ]
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from myapp.views import Listdoc, Detaildoc, CreateDoc, DeleteDoc
+from myapp.views import RegisterView, LoginView, DashboardView
+
+
+from myapp.views import CreateChatMessage,Listuser_message
+
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/api/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/api/auth/login/', LoginView.as_view(), name='login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/dashboard/', DashboardView.as_view(), name='dashboard'),
+
+    path('ap/', Listdoc.as_view(), name='listdoc'),
+    path('ap/<int:pk>/', Detaildoc.as_view(), name='detaildoc'),
+    path('ap/create/', CreateDoc.as_view(), name='createdoc'),
+    path('ap/delete/<int:pk>/', DeleteDoc.as_view(), name='deletedoc'),
+    path('chatlist/',Listuser_message.as_view(), name='chatlist'),
+    path('chat/', CreateChatMessage.as_view(), name='chat'),
+]
+
+# Add this block to serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ myapp/aidiscription.py
@@ -0,0 +1,47 @@
+import google.generativeai as genai
+import fitz  # PyMuPDF
+# Configure your Gemini API Key
+
+# from .views import Listdoc
+from .models import Doc
+from .models import ChatMessage
+from django.contrib.auth.models import User
+
+
+def generate_ai_description_from_pdf_text(text):
+    genai.configure(api_key="AIzaSyBzAUSFhBrF4HPUUwxObFnXItzBf5ie0uM")
+    client = genai.GenerativeModel("gemini-2.0-flash")
+    prompt = f"Read this PDF content and summarize about the file it in 2-3 professional sentences :\n\n{text[:8000]}"  # Truncate if long
+    response = client.generate_content(prompt)
+    return response.text
+
+def generate_ai_user_response(usser,text):
+    user_docs  = Doc.objects.filter(user=usser).values_list('Text', flat=True)
+    concatenated_text = ' '.join(filter(None, user_docs))
+
+    message_docs  = ChatMessage.objects.filter(user=usser).values_list('user_message', flat=True)
+    message_text = ' '.join(filter(None, message_docs)) + text
+    augmented_prompt = f"""
+        Read this PDF content and find the answer to the question in 2-3 professional sentences.
+
+        Context:  
+        {concatenated_text}  
+
+        Query: {message_text}
+        """
+
+    # DAT = Listdoc.as_view()
+
+    
+    genai.configure(api_key="AIzaSyBzAUSFhBrF4HPUUwxObFnXItzBf5ie0uM")
+    client = genai.GenerativeModel("gemini-2.0-flash")
+    prompt = f"{augmented_prompt}"  # Truncate if long
+    response = client.generate_content(prompt)
+    return response.text
+
+def extract_text_from_pdf(file_path):
+    text = ""
+    with fitz.open(file_path) as doc:
+        for page in doc:
+            text += page.get_text()
+    return text.strip()
